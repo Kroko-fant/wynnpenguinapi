ced5479cd44d34fc8d403ae802231582707096b9
Kroko-fant <schrottsuperdeluxe@gmail.com>
2021-11-06 21:52:50 +0100

    item handling

---
+++ main/handlers/ItemHandler.go
@@ -0,0 +1,99 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+	"wynnpenguinapi/main/datastructures"
+)
+
+var items []datastructures.Item
+var itemVersion float64
+
+func InitializeItems() {
+	jsonFile, err := os.Open("data/items.json")
+	if err != nil {
+		panic(err)
+	}
+	defer func(jsonFile *os.File) {
+		err := jsonFile.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var data datastructures.Itemrequest
+	err = json.Unmarshal(byteValue, &data)
+	if err != nil {
+		panic(err)
+	}
+	//Iterate over all items
+	for _, element := range data.Items {
+		items = append(items, element)
+	}
+	itemVersion = data.Request.Version
+	fmt.Print(len(items))
+	fmt.Println(" items loaded")
+}
+
+// ItemsIndex A handler to give out all the items stored in the api.
+func ItemsIndex(w http.ResponseWriter, r *http.Request) {
+	itemresponse := datastructures.Itemrequest{Items: items,
+		Request: datastructures.RequestStruct{Timestamp: time.Now().Unix(), Version: itemVersion}}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(itemresponse); err != nil {
+		_, err := fmt.Fprint(w, "Internal Server error")
+		if err != nil {
+			return
+		}
+	}
+}
+
+// SearchItem A handler to give out a specific item or multiple items up to 10 items at a time.
+// Reasoning: Requesting a whole build is now a lot easier
+// Multiple Items should be seperated by a ',' the list can be in {} or not.
+func SearchItem(w http.ResponseWriter, r *http.Request) {
+	searches := mux.Vars(r)
+	searches["name"] = strings.Replace(searches["name"], "{", "", 1)
+	searches["name"] = strings.Replace(searches["name"], "}", "", 1)
+	requested := strings.Split(searches["name"], ",")
+
+	if len(requested) > 10 {
+		_, err := fmt.Fprintln(w, "Too many requests")
+		if err != nil {
+			return
+		}
+		return
+	}
+
+	var result []datastructures.Item
+
+	for _, name := range requested {
+		for index, i := range items {
+			if i.Name == name {
+				result = append(result, items[index])
+			}
+		}
+	}
+
+	if len(result) <= 0 {
+		_, err := fmt.Fprint(w, "empty")
+		if err != nil {
+			return
+		}
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	itemresponse := datastructures.Itemrequest{Items: result,
+		Request: datastructures.RequestStruct{Timestamp: time.Now().Unix(), Version: itemVersion}}
+
+	if err := json.NewEncoder(w).Encode(itemresponse); err != nil {
+		panic(err)
+	}
+
+}
--- main/main.go
+++ main/main.go
@@ -1,63 +1,32 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strings"
-	"time"
-	"wynnpenguinapi/main/datastructures"
+	"wynnpenguinapi/main/handlers"
 )
-
-var items []datastructures.Item
-var version float32
 
 //TODO: Write Files
 //file, _ := json.Marshal(data)
-//	_ = ioutil.WriteFile("server/main/data/api.json", file, 0644)
+//	_ = ioutil.WriteFile("server/main/data/items.json", file, 0644)
 
 func main() {
 	fmt.Println("Initializing server...")
-	//
-	jsonFile, err := os.Open("data/api.json")
-	if err != nil {
-		panic(err)
-	}
-	defer func(jsonFile *os.File) {
-		err := jsonFile.Close()
-		if err != nil {
-			fmt.Println(err)
-		}
-	}(jsonFile)
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	var data datastructures.DictWithItemList
-	err = json.Unmarshal(byteValue, &data)
-	if err != nil {
-		panic(err)
-	}
-	//Iterate over all items
-	for _, element := range data.Items {
-		items = append(items, element)
-	}
-	fmt.Print("Data loaded ")
-	fmt.Print(len(items))
-	fmt.Println(" items loaded")
-	version = 1.0
+	//Initialize items
+	handlers.InitializeItems()
 
 	fmt.Println("Establishing connections")
 
 	// Initialize routes of the server
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index).Methods("GET")
-	router.HandleFunc("/items", ItemsIndex).Methods("GET")
-	router.HandleFunc("/cache/get/itemList", ItemsIndex).Methods("GET")
-	router.HandleFunc("/itemDB", ItemsIndex).Methods("GET")
-	router.HandleFunc("/items/search={name}", SearchItem).Methods("GET")
-	router.HandleFunc("/itemDB/search={name}", SearchItem).Methods("GET")
+	router.HandleFunc("/items", handlers.ItemsIndex).Methods("GET")
+	router.HandleFunc("/cache/get/itemList", handlers.ItemsIndex).Methods("GET")
+	router.HandleFunc("/itemDB", handlers.ItemsIndex).Methods("GET")
+	router.HandleFunc("/items/search={name}", handlers.SearchItem).Methods("GET")
+	router.HandleFunc("/itemDB/search={name}", handlers.SearchItem).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -68,60 +37,4 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		return
 	}
-}
-
-// ItemsIndex A handler to give out all the items stored in the api.
-func ItemsIndex(w http.ResponseWriter, r *http.Request) {
-	itemresponse := datastructures.DictWithItemList{Items: items,
-		Request: datastructures.Request{Timestamp: time.Now().Unix(), Version: version}}
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(itemresponse); err != nil {
-		_, err := fmt.Fprint(w, "Internal Server error")
-		if err != nil {
-			return
-		}
-	}
-}
-
-// SearchItem A handler to give out a specific item or multiple items up to 10 items at a time.
-// Reasoning: Requesting a whole build is now a lot easier
-// Multiple Items should be seperated by a ',' the list can be in {} or not.
-func SearchItem(w http.ResponseWriter, r *http.Request) {
-	searches := mux.Vars(r)
-	searches["name"] = strings.Replace(searches["name"], "{", "", 1)
-	searches["name"] = strings.Replace(searches["name"], "}", "", 1)
-	requested := strings.Split(searches["name"], ",")
-
-	if len(requested) > 10 {
-		_, err := fmt.Fprintln(w, "Too many requests")
-		if err != nil {
-			return
-		}
-		return
-	}
-
-	var result datastructures.List
-
-	for _, name := range requested {
-		for index, i := range items {
-			if i.Name == name {
-				result = append(result, items[index])
-			}
-		}
-	}
-
-	if len(result) <= 0 {
-		_, err := fmt.Fprint(w, "empty")
-		if err != nil {
-			return
-		}
-		return
-	}
-	itemresponse := datastructures.DictWithItemList{Items: result,
-		Request: datastructures.Request{Timestamp: time.Now().Unix(), Version: version}}
-
-	if err := json.NewEncoder(w).Encode(itemresponse); err != nil {
-		panic(err)
-	}
-
 }
